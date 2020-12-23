e0782990909c5297265f887590a6f653eb17a48e
jiho-s <40516042+jiho-s@users.noreply.github.com>
2020-12-23 11:35:22 +0900

    Create Member

---
+++ src/main/java/me/jiho/oauthdemo/domain/member/Member.java
@@ -0,0 +1,30 @@
+package me.jiho.oauthdemo.domain.member;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jiho.oauthdemo.domain.common.BaseEntity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+
+    private String name;
+
+    private String email;
+
+    @ElementCollection
+    @Enumerated(value = EnumType.ORDINAL)
+    Set<MemberRole> role;
+
+    @Builder
+    public Member(String name, String email, Set<MemberRole> role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+}
---
+++ src/main/java/me/jiho/oauthdemo/domain/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package me.jiho.oauthdemo.domain.member;
+
+import me.jiho.oauthdemo.domain.common.BaseRepository;
+
+public interface MemberRepository extends BaseRepository<Member> {
+}
