58962ebb6a34403fd35345c91bfee759027af998
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-01-25 14:17:43 +0900

    feat: Create BaseEntity.java

---
+++ SpringDataJpa/src/main/java/me/jiho/springdatajpa/common/BaseEntity.java
@@ -0,0 +1,37 @@
+package me.jiho.springdatajpa.common;
+
+import lombok.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * @author jiho
+ * @since 2021/01/25
+ */
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
