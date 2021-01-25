a1fb32c6ad2e5f6276843ed4444647082219a305
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-01-25 14:18:06 +0900

    feat: Create BaseTimeEntity.java

---
+++ SpringDataJpa/src/main/java/me/jiho/springdatajpa/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package me.jiho.springdatajpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author jiho
+ * @since 2021/01/25
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
