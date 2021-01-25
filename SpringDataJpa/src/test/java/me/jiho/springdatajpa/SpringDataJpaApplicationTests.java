9ada92d1bd6968d8bfb653c1623c8c191d83eed8
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-01-25 13:57:38 +0900

    Initial spring-data-jpa

---
+++ SpringDataJpa/src/test/java/me/jiho/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jiho.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
