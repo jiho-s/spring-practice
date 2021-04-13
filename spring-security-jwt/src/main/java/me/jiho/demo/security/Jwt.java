9d2610d856e635253f46abafbcdece3ae9c20d78
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-04-13 13:48:40 +0900

    feat: Update Jwt
    add refreshRangeMills
    add refreshToken

--- spring-security-jwt/src/main/java/me/jiho/demo/security/Jwt.java
+++ spring-security-jwt/src/main/java/me/jiho/demo/security/Jwt.java
@@ -4,12 +4,15 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.RequiredArgsConstructor;
 import me.jiho.demo.configure.JwtTokenProperty;
 import me.jiho.demo.member.Member;
+import me.jiho.demo.member.MemberRole;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Set;
 
 /**
  * @author jiho
@@ -24,27 +27,51 @@ public class Jwt {
 
     private final int expirySeconds;
 
+    private final long refreshRangeMills;
+
     private final Algorithm algorithm;
 
     private final JWTVerifier jwtVerifier;
+
 
     public Jwt(JwtTokenProperty jwtTokenProperty) {
         this.issuer = jwtTokenProperty.getIssuer();
         this.clientSecret = jwtTokenProperty.getClientSecret();
         this.expirySeconds = jwtTokenProperty.getExpirySeconds();
+        this.refreshRangeMills = jwtTokenProperty.getRefreshRangeMills();
         this.algorithm = Algorithm.HMAC512(clientSecret);
         this.jwtVerifier = JWT.require(algorithm)
                 .withIssuer(issuer)
                 .build();
     }
 
-    public String generateToken(Member member) {
+    public String generateToken(Claims claims) {
         Date now = new Date();
-        JWT.create()
+        return JWT.create()
                 .withIssuer(issuer)
                 .withIssuedAt(now)
-                .withClaim("id", member.getId())
-                .withClaim("name", member.getName())
-                .with
+                .withExpiresAt(expirySeconds > 0 ? new Date(now.getTime() + expirySeconds * 1_000L) : null)
+                .withClaim("id", claims.getId())
+                .withArrayClaim("roles", claims.getRoles())
+                .sign(algorithm);
+    }
+
+    public String refreshToken(String token) throws JWTVerificationException {
+        Claims claims = verify(token);
+        claims.eraseDates();
+        return generateToken(claims);
+    }
+
+    public boolean canRefresh(Claims claims) {
+        Date exp = claims.getExp();
+        if (exp == null) {
+            return false;
+        }
+        long remain = exp.getTime() - System.currentTimeMillis();
+        return remain < refreshRangeMills;
+    }
+
+    public Claims verify(String token) throws JWTVerificationException {
+        return new Claims(jwtVerifier.verify(token));
     }
 }
