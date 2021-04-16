363680099ee7d55916c82ac2bf881bc4339ccd61
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-04-16 12:24:41 +0900

    feat: Update ServiceConfigure
    add default member

--- spring-security-jwt/src/main/java/me/jiho/demo/configure/ServiceConfigure.java
+++ spring-security-jwt/src/main/java/me/jiho/demo/configure/ServiceConfigure.java
@@ -1,6 +1,10 @@
 package me.jiho.demo.configure;
 
-import me.jiho.demo.security.Jwt;
+import me.jiho.demo.member.Member;
+import me.jiho.demo.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -17,5 +21,25 @@ public class ServiceConfigure {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            MemberRepository memberRepository;
+
+            @Autowired
+            PasswordEncoder passwordEncoder;
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Member jiho = memberRepository.save(Member.builder()
+                        .email("test@email.com")
+                        .password(passwordEncoder.encode("1234"))
+                        .name("jiho")
+                        .build());
+                System.out.println(jiho.getEmail());
+            }
+        };
     }
 }
